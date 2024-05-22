daf89d432a19abfe379b4ba0366826c119966e1f
aschimmichanga <ashnasrivastava1@gmail.com>
2024-05-22 00:46:42 -0400

    add deals and discounts

--- models.cjs
+++ models.cjs
@@ -1,4 +1,19 @@
 const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({ name: String, email: String, age: Number });
+
+const userSchema = new mongoose.Schema({ name: String, password: String });
 const User = mongoose.model('User', userSchema);
-module.exports = User;
+
+const discountSchema = new mongoose.Schema({
+    description: String,
+    expiryDate: Date
+});
+
+const dealSchema = new mongoose.Schema({
+    name: String,
+    discounts: [discountSchema],
+    imageUrl: String,
+    details: String
+});
+
+const Deal = mongoose.model('Deal', dealSchema);
+module.exports = { User, Deal };
